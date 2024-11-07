f349d5128d7efac9babd1e846d452b30c93c4fe4
Ngat Nguyen <ngat.nguyen@rikai.technology>
2024-11-07 13:38:07 +0700

    add lb firebase and ui login

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:future_app/view/login_view.dart';
 
 void main() {
+  Future<void> initializeDefault() async {
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+            apiKey: "AIzaSyCMFTMVxVGOAFt55rjGz2rR6FO453TIbvE",
+            authDomain: "futureapp-d8afd.firebaseapp.com",
+            projectId: "futureapp-d8afd",
+            storageBucket: "futureapp-d8afd.firebasestorage.app",
+            messagingSenderId: "1023034779511",
+            appId: "1:1023034779511:web:dcec0356151fc890c36372",
+            measurementId: "G-M61S9535Q3"));
+  }
+
   runApp(const MyApp());
 }
 
@@ -20,6 +33,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
-
--- lib/view/login_view.dart
+++ lib/view/login_view.dart
@@ -1,10 +1,64 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.all(30),
+        child: Column(children: [
+          SizedBox(
+            height: 135,
+          ),
+          Image.asset(
+            'assets/images/Rectangle.png',
+            height: 330,
+            width: 352,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Color(0xffc0c0c0), width: 2),
+                  borderRadius: BorderRadius.all(Radius.circular(10))),
+             
+              child: TextField(
+                  decoration: InputDecoration(
+                      border: InputBorder.none, hintText: 'Email'))),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Color(0xffc0c0c0), width: 2),
+                  borderRadius: BorderRadius.all(Radius.circular(10))),
+             
+              child: TextField(
+                  decoration: InputDecoration(
+                      suffixIcon: Icon(Icons.remove_red_eye),
+                      border: InputBorder.none,
+                      hintText: 'Password'))),
+          SizedBox(
+            height: 6,
+          ),
+          Align(
+              alignment: Alignment.centerRight, child: Text('Quên mật khẩu?')),
+              SizedBox(height: 6,),
+          Container(
+            height: 52,
+            width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                  color: Color(0xff3498DB),
+                  borderRadius: BorderRadius.all(Radius.circular(10))),
+                  child: Center(child: Text('Login',style: TextStyle(color: Colors.white),)),
+              )
+        ]),
+      ),
+    );
   }
 }
